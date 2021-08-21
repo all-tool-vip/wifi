d9a94f57d8977dc620e525129a480ac8b4243c20
all-tool-vip <88178692+all-tool-vip@users.noreply.github.com>
2021-08-21 18:50:19 +0700

    Add files via upload

---
+++ wifi.sh
@@ -0,0 +1,58 @@
+clear
+#kode warna. 
+MERAH="\033[31;1m"
+HIJAU="\033[32;1m"
+echo -e $MERAH
+figlet "KingSFreamWork Attack WIFI PW"
+
+echo "##################################" | lolcat
+echo "USERNAME : KingSFreamWork" | lolcat
+echo "YOUTUBE  : KingSFreamWork" | lolcat
+echo "USER : TERMUX HACK OFFICIAL" | lolcat
+echo "SUBS : SUBSCRIBE KingSFrame" | lolcat
+echo "##################################" | lolcat
+clear
+echo "SUBSCRIBE KingSFrameWork"
+sleep 2
+xdg-open https://youtube.com/channel/UCj78sxhTeVRJOtViz_3yqQA &
+sleep 1
+echo "##################################" | lolcat
+echo "USERNAME : KingSFreamWork" | lolcat
+echo "YOUTUBE  : KingSFreamWork" | lolcat
+echo "USER : TERMUX HACK OFFICIAL" | lolcat
+echo "SUBS : SUBSCRIBE KingSFrame" | lolcat
+echo "##################################" | lolcat
+
+read -p "you will install wifi Script N/Y ? :"
+sleep 2
+termux-setup-storage
+pkg install php
+pkg install wget
+gem install lolcat
+pkg install figlet
+pkg install curl
+pkg install bash
+echo "All successfully installed wifi"
+
+echo "cd /sdcard/Connect_Hack_Wifi_base.apk"
+sleep 5
+echo "2% [1 perl 442 kB/14.3 MB 3%]"
+sleep 1
+echo "4% [1 perl 762 kB/14.3 MB 5%]"
+sleep 1
+echo "6% [1 perl 1097 kB/14.3 MB 8%]" | lolcat
+echo "8% [1 perl 1458 kB/14.3 MB 10%]" | lolcat
+echo "11% [1 perl 1884 kB/14.3 MB 13%]" | lolcat
+echo "20% [1 perl 3506 kB/14.3 MB 24%]     554 kB/s 19s" | lolcat
+echo "23% [1 perl 4128 kB/14.3 MB 29%]     554 kB/s 18s" | lolcat
+echo "49% [1 perl 8781 kB/14.3 MB 61%]     554 kB/s 10s" | lolcat
+echo "52% [1 perl 9322 kB/14.3 MB 65%]      554 kB/s 9s" | lolcat
+echo "55% [1 perl 9896 kB/14.3 MB 69%]      554 kB/s 8s" | lolcat
+echo "62% [1 perl 11.1 MB/14.3 MB 78%]     1164 kB/s 2s" | lolcat
+echo "69% [1 perl 12.3 MB/14.3 MB 86%]     1164 kB/s 1s" | lolcat
+echo "72% [1 perl 12.9 MB/14.3 MB 90%]     1164 kB/s 1s" | lolcat
+echo"76% [1 perl 13.6 MB/14.3 MB 95%]     1164 kB/s 0s" | lolcat
+
+echo "It worked, please check the sdcard"
+exit
+
